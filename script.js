20c63b223f6a69f9acdecc949f1d14aa740b9b41
naledimathebula <naledimathebula18@gmail.com>
2026-09-09 15:55:03 +0200

    colorstop

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function  drawlobes( ctx,w,h, time = 0) {
     ctx.globalCompositeOperation = "multiply";
     lobes.forEach( 1 => {
     const g = ctx.createRadialGradient(l.cx, cy, 0, l.cx, cy, w * 0.3  * pulse);
+    g.addColorStop(0, l.color);
+    g.addColorStop(0.5, "rgba(200,120,220,0.5)");
+    g.addColorStop(1, "rgba(255,255,255,0)");
+    ctx.beginPath();
+    ctx.arc(l.cx, cy, w * 0.3 * pulse, 0, Math.PI * 2);
+    ctx.fill();
+    });
+}
